3e238092f52ba6a5a177015595ecd529addf337b
kumarnutan204 <kumarnutan204@gmail.com>
2024-02-14 17:46:52 +0530

    [Nutan] Added:UC2- Added contact to the addressbook

--- AddressBook.py
+++ AddressBook.py
@@ -1,28 +1,41 @@
 class AddressBook:
-    pass
+    def __init__(self):
+        self.AddressBook_dict={}
+        
+    def add_contact(self,Contact_obj):
+        FullName=Contact_obj.Contact_dict['first']+Contact_obj.Contact_dict['last']
+        self.AddressBook_dict.update({FullName:Contact_obj})
+        print("Contact added in Address book")
+        
+    def ShowAddressBook(self):
+        print("Below is the Address Book: ")
+        print(self.AddressBook_dict)
 
 class Contact:
     def __init__(self,first,last,address, city,state,zip,phone,email):
-        self.first=first
-        self.last=last
-        self.address=address
-        self.city=city
-        self.state=state
-        self.zip=zip
-        self.phone=phone
-        self.email=email
+        self.Contact_dict={'first':first,'last':last,'address':address,'city':city,'state':state,'zip':zip,'phone':phone,'email':email}
+        # self.first=first
+        # self.last=last
+        # self.address=address
+        # self.city=city
+        # self.state=state
+        # self.zip=zip
+        # self.phone=phone
+        # self.email=email
     def show_contact(self):
-        print(f"first name = {self.first}")
-        print(f"last name = {self.last}")
-        print(f"address = {self.address}")
-        print(f"city = {self.city}")
-        print(f"state = {self.state}")
-        print(f"zip = {self.zip}")
-        print(f"phone number = {self.phone}")
-        print(f"email = {self.email}")
+        print(self.Contact_dict)
+        # print(f"first name = {self.first}")
+        # print(f"last name = {self.last}")
+        # print(f"address = {self.address}")
+        # print(f"city = {self.city}")
+        # print(f"state = {self.state}")
+        # print(f"zip = {self.zip}")
+        # print(f"phone number = {self.phone}")
+        # print(f"email = {self.email}")
  
 
 def addCont():
+    Address=AddressBook()
     first=input("Enter the first name: ")
     last=input("Enter the last name: ")
     address=input("Enter the address: ")
@@ -34,7 +47,16 @@ def addCont():
 
     cont=Contact(first,last,address,city,state,zip,phone,email)
     cont.show_contact()
+    choice=input("Add it in AddressBook? Type 'yes' or 'no'  : ")
+    if choice=='yes':
+        Address.add_contact(cont)
+        Address.ShowAddressBook()
+    else:
+        exit()
+    
     
 if __name__=='__main__':
         addCont()
+        
+        
         
